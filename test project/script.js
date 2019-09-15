448c350c6411ba2063dac23dc99863f1f7a414a9
ManYaka <sergeyrakov_93@mail.ru>
2019-09-15 16:21:17 +0500

    Задание 3 практика

--- test project/script.js
+++ test project/script.js
@@ -1,6 +1,5 @@
 let money,
     time,
-    tempBuf,
     appData = {
         budget: money,
         timeData: time,
@@ -10,19 +9,37 @@ let money,
         savings: false
     };
 
-money = prompt("Ваш бюджет на месяц?");
+money = +prompt("Ваш бюджет на месяц?", "");
 appData.budget = money;
-time = prompt("Введите дату в формате YYYY-MM-DD");
+time = prompt("Введите дату в формате YYYY-MM-DD", "");
 appData.timeData = time;
 
-tempBuf = prompt("Введите обязательную статью расходов в этом месяце");
-appData.expenses[tempBuf] = prompt("Во сколько обойдётся?");
-tempBuf = prompt("Введите обязательную статью расходов в этом месяце");
-appData.expenses[tempBuf] = prompt("Во сколько обойдётся?");
+for (let i = 0; i < 2; ++i) {
+    let keyName = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        cost = +prompt("Во сколько обойдётся?", "");
+    if (typeof (keyName) != null && typeof (cost) != null &&
+        keyName != "" && cost != "") {
+        appData.expenses[keyName] = cost;
+    } else {
+        alert("Cтроки пустые...");
+    }
+}
 
-alert("Ваш бюджет на один день: " + (money / 30));
+appData.moneyPerDay = appData.budget / 30;
 
-for (let key in appData.expenses) {
+alert("Ваш бюджет на один день: " + (appData.moneyPerDay));
+
+if (appData.moneyPerDay < 100) {
+    alert("У вас низкий уровень дохода :(");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 500) {
+    alert("У вас средний уровень дохода");
+} else if (appData.moneyPerDay >= 500) {
+    alert("У вас высокий уровень дохода");
+} else {
+    alert("Что-то не так в if...");
+}
+
+/* for (let key in appData.expenses) {
     alert(key + ": " + appData.expenses[key]);
     break;
-}
+} */
